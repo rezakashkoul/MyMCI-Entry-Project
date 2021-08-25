d836543d1ba941f22f7bb22c0b0416a6f8a2e041
Reza Kashkoul <rezakashkoul@gmail.com>
2021-08-26 00:16:15 +0430

    first runing

---
+++ MyMCI Entry Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMCI Entry Project
+//
+//  Created by Reza Kashkoul on 6/3/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
